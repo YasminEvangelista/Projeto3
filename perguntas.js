13a1e44d015f4199b218389d3024c7e6bf7eed25
YasminEvangelista <00001093526683sp@al.educacao.sp.gov.br>
2024-11-27 17:37:13 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Educação Física',
+    'Por que o alongamento é importante após o exercício?',
+    'O alongamento é importante para ajudar na recuperação muscular, melhorar a flexibilidade e reduzir o risco de dores e lesões.'
+);
+
+criaCartao(
+    'Arte',
+    'Qual é a principal característica do cubismo?',
+    'O cubismo é caracterizado pela representação de formas geométricas e pela fragmentação dos objetos em diferentes perspectivas simultaneamente.'
+);
+
+criaCartao(
+    'Biologia',
+    'Qual é a função dos ribossomos na célula?',
+    'Os ribossomos são responsáveis pela síntese de proteínas, utilizando informações do RNA mensageiro.'
+);
+
+criaCartao(
+    'Filosofia',
+    'O que significa a frase “Penso, logo existo”?',
+    'Essa frase, de René Descartes, significa que a capacidade de pensar é a prova fundamental da existência do indivíduo.'
+);
